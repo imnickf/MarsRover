826a1a6a714e3d3e859cf14c8e8c6efd9204029d
jlt94 <jlt94@9928679a-b9dc-4c3d-a31b-638ea8c60b7f>
2016-04-20 17:32:01 +0000

    Added cliff sensors to movement.c

--- FinalProject/FinalProject/movement.c
+++ FinalProject/FinalProject/movement.c
@@ -1,18 +1,73 @@
 #include "movement.h"
+#include "util.h"
 
 void move_forward(oi_t *sensor, int centimeters) 
 {
 	int sum = 0;
 	oi_set_wheels(200, 200); // move forward
 	while (sum < centimeters * 10) {
-		if (sensor->bumper_left || sensor->bumper_right || sensor->cliff_left || sensor->cliff_right || sensor->cliff_frontleft || sensor ->cliff_frontright) {
-			move_backward(sensor, 15);
-			//turn_clockwise(sensor, 87);
-			//move_forward(sensor, 25);
-			//turn_counterClockwise(sensor, 87);
-			//move_forward(sensor, (centimeters - (sum / 10)));
+		if(sensor->bumper_left==1){
+			char message[]="bumper_left";
+			for(int i=0;i<strlen(message);i++){USART_Transmit(message[i]);}
+			move_backward(sensor, 10);
 			break;
 		}
+		if(sensor->bumper_right==1){
+			char message[]="bumper_right";
+			for(int i=0;i<strlen(message);i++){USART_Transmit(message[i]);}
+			move_backward(sensor, 10);
+			break;
+		}
+		if(sensor->cliff_left==1){
+			char message[]="cliff_leftCLIFF";
+			for(int i=0;i<strlen(message);i++){USART_Transmit(message[i]);}
+			move_backward(sensor, 10);
+			break;
+		}
+		if(sensor->cliff_frontleft==1){
+			char message[]="cliff_frontleftCLIFF";
+			for(int i=0;i<strlen(message);i++){USART_Transmit(message[i]);}
+			move_backward(sensor, 10);
+			break;
+		}
+		if(sensor->cliff_right==1){
+			char message[]="cliff_rightCLIFF";
+			for(int i=0;i<strlen(message);i++){USART_Transmit(message[i]);}
+			move_backward(sensor, 10);
+			break;
+		}
+		if(sensor->cliff_frontright==1){
+			char message[]="cliff_frontrightCLIFF";
+			for(int i=0;i<strlen(message);i++){USART_Transmit(message[i]);}
+			move_backward(sensor, 10);
+			break;
+		}
+		if(sensor->cliff_left_signal>600){
+			char message[]="cliff_left_signal>600WALL";
+			for(int i=0;i<strlen(message);i++){USART_Transmit(message[i]);}
+			move_backward(sensor, 10);
+			break;
+		}
+		if(sensor->cliff_frontleft_signal>600){
+			char message[]="cliff_frontleft_signal>600WALL";
+			for(int i=0;i<strlen(message);i++){USART_Transmit(message[i]);}
+			move_backward(sensor, 10);
+			break;
+		}
+		if(sensor->cliff_right_signal>350){
+			char message[]="cliff_right_signal>350WALL";
+			for(int i=0;i<strlen(message);i++){USART_Transmit(message[i]);}
+			move_backward(sensor, 10);
+			break;
+		}
+		if(sensor->cliff_frontright_signal>350){
+			char message[]="cliff_frontright_signal>350WALL";
+			for(int i=0;i<strlen(message);i++){USART_Transmit(message[i]);}
+			move_backward(sensor, 10);
+			break;
+		}		
+		
+		
 		oi_update(sensor);
 		sum += sensor->distance;
 	}
